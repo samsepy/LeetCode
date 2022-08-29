c4a180e6bf7c7245e2b749439b547d520cd55ba6
Hajime Todo <samsepylot@gmail.com>
2022-08-29 18:50:02 +0900

    703. Kth Largest Element in a Stream 改良

--- writeups/703_Kth_Largest_Element_in_a_Stream/ruby/main.rb
+++ writeups/703_Kth_Largest_Element_in_a_Stream/ruby/main.rb
@@ -3,11 +3,20 @@ class KthLargest
 
   def initialize(k, nums)
     @k = k
-    @nums  = nums
+    @nums  = nums.sort!.reverse!
   end
 
   def add(val)
-    nums.push(val).sort!.reverse!
+    if k > nums.length
+      @nums = nums.push(val).sort!.reverse!
+
+      return nums[k-1]
+    end
+    
+    if val > nums[k-1]
+      nums.unshift(val)
+      @nums = nums[0..(k-1)].sort.reverse + nums[k..]
+    end
 
     nums[k-1]
   end
