9d18ebd12c32f64b876455eb1a6856961931e291
Hajime Todo <samsepylot@gmail.com>
2022-08-30 03:40:35 +0900

    1. Two Sum ハッシュテーブル使って正解

--- writeups/1_Two_Sum/ruby/main.rb
+++ writeups/1_Two_Sum/ruby/main.rb
@@ -4,10 +4,13 @@
 def two_sum(nums, target)
   return [0, 1] if nums.length == 2
 
-  nums.each_with_index do |x, i|
-    nums[i+1..].each_with_index do |y, j|
-      return [i, i+j+1] if (x + y) == target
-    end
+  h = {}
+  nums.each_with_index do |num, i|
+    h[i] = num
+  end
+
+  nums.each_with_index do |num, i|
+    h[i] = nil
+    return [i, h.key(target - num)] if h.value?(target - num)
   end
 end
-
