566daa472b9610872aad2db86f8ba5d704fcbad9
Hajime Todo <samsepylot@gmail.com>
2022-09-01 02:25:08 +0900

    53. Maximum Subarray メモ化使って正解

--- writeups/53_Maximum_Subarray/ruby/main.rb
+++ writeups/53_Maximum_Subarray/ruby/main.rb
@@ -1,15 +1,9 @@
-# @param {Integer[]} nums
-# @return {Integer}
 def max_sub_array(nums)
   max_sum = nums.first
-  nums.each_with_index do |_, i|
-    nums.each_with_index do |_, j|
-      break if i + j > nums.length - 1
-
-      sum = nums[i..i+j].sum
-      max_sum = sum if sum > max_sum
-    end
+  for i in 1...nums.length do
+    nums[i] = [nums[i - 1] + nums[i], nums[i]].max_sum
+    max_sum = [max_sum, nums[i]].max
   end
-  
+
   max_sum
 end
