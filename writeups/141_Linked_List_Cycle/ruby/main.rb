b367a06c9d690fd65405242aba403ed076aa7dde
Hajime Todo <samsepylot@gmail.com>
2022-09-01 13:01:29 +0900

    141. Linked List Cycle とりあえず解答

---
+++ writeups/141_Linked_List_Cycle/ruby/main.rb
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @return {Boolean}
+def hasCycle(head)
+  current_node = head
+  checked_nodes = []
+  while current_node do
+    if checked_nodes.include?(current_node)
+      return true
+    else
+      checked_nodes.push(current_node)
+      current_node = current_node.next
+    end
+  end
+  
+  false
+end
