0853bfb4445070618d473e6a897a696e6f66a8f4
Hajime Todo <samsepylot@gmail.com>
2022-09-04 14:42:43 +0900

    fix

--- writeups/1_Two_Sum/ruby/main.rb
+++ writeups/1_Two_Sum/ruby/main.rb
@@ -4,8 +4,9 @@
 def two_sum(nums, target)
   h = {}
   nums.each_with_index do |num, i|
-    if h.has_key?(target - num)
-      return [i, h.fetch(target - num)]
+    diff = target - num
+    if h.has_key?(diff)
+      return [h.fetch(diff), i]
     else
       h[num] = i
     end
