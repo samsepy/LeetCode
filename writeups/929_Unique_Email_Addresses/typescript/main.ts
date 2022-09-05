513aa02fc4ab983ac135ca15d058199c72dbd438
Hajime Todo <samsepylot@gmail.com>
2022-09-05 19:44:04 +0900

    929. Unique Email Addresses typescriptで解答

---
+++ writeups/929_Unique_Email_Addresses/typescript/main.ts
@@ -0,0 +1,13 @@
+function numUniqueEmails(emails: string[]): number {
+  const uniqueEmails = new Set<string>();
+  emails.forEach((x) => {
+    const [userId, domain]: string[] = x.split("@");
+    const plusIndex: number = userId.indexOf("+");
+    const originUserId: string = userId
+      .substr(0, plusIndex === -1 ? undefined : plusIndex)
+      .replace(/\./g, "");
+    uniqueEmails.add(`${originUserId}@${domain}`);
+  });
+
+  return uniqueEmails.size;
+}
