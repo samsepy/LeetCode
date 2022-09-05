6e18f3b22e04307d0865a169048474048818aa37
Hajime Todo <samsepylot@gmail.com>
2022-09-05 20:48:13 +0900

    fix

--- writeups/387_First_Unique_Character_in_a_String/typescript/main.ts
+++ writeups/387_First_Unique_Character_in_a_String/typescript/main.ts
@@ -1,10 +1,10 @@
 function firstUniqChar(s: string): number {
   const h = new Map<string, number>();
   for (let i = 0; i < s.length; i++) {
-    if (!h.has(s[i])) {
-      h.set(s[i], 1);
-    } else {
+    if (h.has(s[i])) {
       h.set(s[i], h.get(s[i]) + 1);
+    } else {
+      h.set(s[i], 1);
     }
   }
   for (let i = 0; i < s.length; i++) {
