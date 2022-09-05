0ca25ac0674d4baf67b24a399d2721dd91acfd73
Hajime Todo <samsepylot@gmail.com>
2022-09-05 21:52:24 +0900

    indexOfを使わず実装できた

--- writeups/387_First_Unique_Character_in_a_String/typescript/main.ts
+++ writeups/387_First_Unique_Character_in_a_String/typescript/main.ts
@@ -2,15 +2,13 @@ function firstUniqChar(s: string): number {
   const h = new Map<string, number>();
   for (let i = 0; i < s.length; i++) {
     if (h.has(s[i])) {
-      h.set(s[i], h.get(s[i]) + 1);
+      h.set(s[i], -1);
     } else {
-      h.set(s[i], 1);
+      h.set(s[i], i);
     }
   }
   for (const [k, v] of h) {
-    if (v == 1) {
-      return s.indexOf(k);
-    }
+    if (v !== -1) return v;
   }
 
   return -1;
