5f5a10f74b7e52409b0ff8d367afddcefdecfbdc
Hajime Todo <samsepylot@gmail.com>
2022-09-08 16:54:28 +0900

    配列を先頭から一つずつ取り出して計算するようにして正解

--- writeups/2_Add_Two_Numbers/typescript/main.ts
+++ writeups/2_Add_Two_Numbers/typescript/main.ts
@@ -14,30 +14,17 @@
   l1: ListNode | null,
   l2: ListNode | null,
 ): ListNode | null {
-  const l1Array: number[] = [];
-  const l2Array: number[] = [];
-  let currentL1Node = l1;
-  let currentL2Node = l2;
-  while (currentL1Node) {
-    l1Array.push(currentL1Node.val);
-    currentL1Node = currentL1Node.next;
-  }
-  while (currentL2Node) {
-    l2Array.push(currentL2Node.val);
-    currentL2Node = currentL2Node.next;
-  }
-  const sumBothList: string = String(
-    Number(l1Array.reverse().join("")) + Number(l2Array.reverse().join("")),
-  )
-    .split("")
-    .reverse()
-    .join("");
-  let sumBothListHead: ListNode = new ListNode(Number(sumBothList[0]));
-  let currentNode: ListNode = sumBothListHead;
-  for (let i = 1; i < sumBothList.length; i++) {
-    currentNode.next = new ListNode(Number(sumBothList[i]));
+  const head = new ListNode();
+  let currentNode = head;
+  let carry: number = 0;
+  while (l1 || l2 || carry) {
+    const sum: number = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    carry = Math.floor(sum / 10);
+    currentNode.next = new ListNode(sum % 10);
     currentNode = currentNode.next;
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
   }
 
-  return sumBothListHead;
+  return head.next;
 }
