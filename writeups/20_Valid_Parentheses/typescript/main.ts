8372ab4bfdd316b6f36fff2467911be44626515c
Hajime Todo <samsepylot@gmail.com>
2022-09-09 18:22:46 +0900

    配列の型がstringだと違和感あるので型を用意

--- writeups/20_Valid_Parentheses/typescript/main.ts
+++ writeups/20_Valid_Parentheses/typescript/main.ts
@@ -1,17 +1,17 @@
 function isValid(s: string): boolean {
   type OpenParentheses = "(" | "{" | "[";
-  const stack: string[] = [];
+  const stack: OpenParentheses[] = [];
   for (let i = 0; i < s.length; i++) {
     if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
-      stack.push(s[i]);
+      stack.push(s[i] as OpenParentheses);
     } else {
       if (stack.length === 0) return false;
 
-      const stackTail: string = stack[stack.length - 1];
+      const stackTail: OpenParentheses = stack[stack.length - 1];
       if (
-        (s[i] === ")" && stackTail == "(") ||
-        (s[i] === "}" && stackTail == "{") ||
-        (s[i] === "]" && stackTail == "[")
+        (s[i] === ")" && stackTail === "(") ||
+        (s[i] === "}" && stackTail === "{") ||
+        (s[i] === "]" && stackTail === "[")
       ) {
         stack.pop();
         continue;
